e62fb673ed9e4f62f573ae1205015b9c66689bd5
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2017-12-17 22:44:22 +0900

    Checking Java 8 installation for Jetty server

--- sample.sh
+++ sample.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-# get download jetty url
-input=app_url.conf
-while IFS= read -r app_url
-do
-  echo "'$app_url'"
-done < "$input"
+# Java install
+sudo apt-get update && sudo apt-get install oracle-java8-jdk
 
-if [ -z ${app_url} ]
-then
-  echo "App not found"
-  read -p "Enter to exit" key_board
-  exit 0
-else
-  echo "App Link: '$app_url'"
-fi
-
-# download jetty
-curl -sS `pwd`/aromacontroller.war "$app_url"
-
-# move zip file
-file=`pwd`/aromacontroller.war
-if [ -f "$file" ]
-then
-  mkdir -p `pwd`/http-controller
-  mv `pwd`/aromacontroller.war `pwd`/http-controller
-else
-  echo
-  echo "==== Download app failed ===="
+if [ $? != 0 ]; then                   
+  echo "Install Java: $? - Unsuccessful"
   read -p "Enter to exit" key_board
   exit 1
 fi
 
+# # get download jetty url
+# input=app_url.conf
+# while IFS= read -r app_url
+# do
+#   echo "'$app_url'"
+# done < "$input"
+
+# if [ -z ${app_url} ]
+# then
+#   echo "App not found"
+#   read -p "Enter to exit" key_board
+#   exit 0
+# else
+#   echo "App Link: '$app_url'"
+# fi
+
+# # download jetty
+# curl -sS `pwd`/aromacontroller.war "$app_url"
+
+# # move zip file
+# file=`pwd`/aromacontroller.war
+# if [ -f "$file" ]
+# then
+#   mkdir -p `pwd`/http-controller
+#   mv `pwd`/aromacontroller.war `pwd`/http-controller
+# else
+#   echo
+#   echo "==== Download app failed ===="
+#   read -p "Enter to exit" key_board
+#   exit 1
+# fi
+
 # wait input to exit program
-echo
-read -p "Done. Enter to exit" key_board
+echo "==== Done ===="
+read -p "Enter to exit" key_board
 exit 0
