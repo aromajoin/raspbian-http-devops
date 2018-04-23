9f4a3534561e50396fa4f13a5df1e1334cbb7dfa
Hanh Tran <tranduchanh.ms@gmail.com>
2018-04-23 15:56:54 +0900

    Update packaging script

--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -5,6 +5,5 @@ set -e
 SELFDIR="`dirname \"$0\"`"
 SELFDIR="`cd \"$SELFDIR\" && pwd`"
 
-echo "$SELFDIR"
 # Run the actual app using the bundle Ruby interpreter
 exec "$SELFDIR/lib/ruby/bin/ruby" "$SELFDIR/lib/app/aroma_on_raspberry.rb"
