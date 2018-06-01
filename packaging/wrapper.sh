2e5a02cb34b4898ad9a0dc8edcbdad91653c3f35
Hanh Tran <tranduchanh.ms@gmail.com>
2018-06-01 14:30:55 +0900

    [Remove] redundant files

--- packaging/wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Figure out where this cript is located
-SELFDIR="`dirname \"$0\"`"
-SELFDIR="`cd \"$SELFDIR\" && pwd`"
-
-# Run the actual app using the bundle Ruby interpreter
-exec "$SELFDIR/lib/ruby/bin/ruby" "$SELFDIR/lib/app/aroma_on_raspberry.rb"
